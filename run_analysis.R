e6937f309c5a4ae9e514798adb9562ef1d832f85
leeooh <leeooh.lopez@gmail.com>
2016-04-01 18:55:31 -0500

    completed file

--- run_analysis.R
+++ run_analysis.R
@@ -58,3 +58,21 @@ subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)",
 selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
 Data<-subset(Data,select=selectedNames)
 
+## 3. Uses descriptive activity names to name the activities in the data set
+#Read descriptive activity names from “activity_labels.txt”
+activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),
+                             header = FALSE)
+
+## 4. Appropriately labels the data set with descriptive variable names
+names(Data)<-gsub("^t", "time", names(Data))
+names(Data)<-gsub("^f", "frequency", names(Data))
+names(Data)<-gsub("Acc", "Accelerometer", names(Data))
+names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
+names(Data)<-gsub("Mag", "Magnitude", names(Data))
+names(Data)<-gsub("BodyBody", "Body", names(Data))
+
+## 5. Creates a second, independent tidy data set and ouput it
+library(plyr);
+Data2<-aggregate(. ~subject + activity, Data, mean)
+Data2<-Data2[order(Data2$subject,Data2$activity),]
+write.table(Data2, file = "tidydata.txt",row.name=FALSE)
